c281d4c6b8a078d59743b3c3485d314670d9f5d7
afeiship <1290657123@qq.com>
2018-05-17 16:35:49 +0800

    issue: add event-value

--- src/components/react-ant-switch.js
+++ src/components/react-ant-switch.js
@@ -5,24 +5,31 @@ import classNames from 'classnames';
 import noop from 'noop';
 import objectAssign from 'object-assign';
 import { Switch } from 'antd';
+import 'next-return-event';
 
-
-export default class extends PureComponent{
+export default class extends Component{
   /*===properties start===*/
   static propTypes = {
     className: PropTypes.string,
     value: PropTypes.bool,
+    onChange: PropTypes.func,
   };
 
   static defaultProps = {
-    value: false
+    value: false,
+    onChange: noop
   };
   /*===properties end===*/
+
+  _onChange = e => {
+    const { onChange } = this.props;
+    onChange( nx.returnEventTarget(e) );
+  };
 
   render(){
     const { className, value, ...props } = this.props;
     return (
-      <Switch checked={value} {...props} className={classNames('react-ant-switch',className)} />
+      <Switch checked={value} { ...props } className={classNames('react-ant-switch',className)} />
     );
   }
 }
