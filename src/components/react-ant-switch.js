f60533d039f0ddd29a05d91123970517e5056ebf
afeiship <1290657123@qq.com>
2018-06-04 11:47:21 +0800

    issue: udpate switch

--- src/components/react-ant-switch.js
+++ src/components/react-ant-switch.js
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import noop from 'noop';
 import objectAssign from 'object-assign';
 import { Switch } from 'antd';
-import 'next-return-event';
+import { returnEventTarget } from 'next-return-event';
 
 export default class extends Component{
   /*===properties start===*/
@@ -23,7 +23,7 @@ export default class extends Component{
 
   _onChange = e => {
     const { onChange } = this.props;
-    onChange( nx.returnEventTarget(e) );
+    onChange( returnEventTarget(e) );
   };
 
   render(){
